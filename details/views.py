3e638a6fd60c47cdb2376987e61efaf015d6d640
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-05-18 11:13:11 +0000

    Updated links in details for film/actor

--- details/views.py
+++ details/views.py
@@ -9,12 +9,11 @@ def film(request):
     film = c.fetchone()
     title = film[1]
     actor_list = film[2]
-    print(actor_list)
     actor_list = actor_list.split(", ")
 
     context = {
         'title': title,
-        'actor_list': actor_list,
+        'list': actor_list,
     }
 
     return render(request, 'details/details.html', context)
@@ -23,17 +22,16 @@ def film(request):
 def actor(request):
     db = sqlite3.connect('films_actors_01.db')
     c = db.cursor()
-    c.execute('''SELECT * FROM films WHERE title="item"''')
-    films = c.fetchall()
-    # c.execute('''SELECT * from actors ''')
-    # actor = c.fetchall()
-    for film in films:
-        title = film[0]
-        actor_list = film[1]
-        actor_list = actor_list.split(", ")
+    c.execute('''SELECT * FROM actors WHERE name="Matt Damon"''')
+    actor = c.fetchone()
+    
+    name = actor[1]
+    film_list = actor[2]
+    film_list = film_list.split(", ")
+
     context = {
-        'title': title,
-        'actor_list': actor_list,
+        'title': name,
+        'list': film_list,
     }
 
     return render(request, 'details/details.html', context)
